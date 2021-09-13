7b9f2293f5ddf2b4823b8b8c831f51284a0b924d
don-kokone <don_kokone@start.bg>
2021-09-13 22:50:23 +0300

    Commite new experiment with js

--- Assets/Javascript/activate-menu.js
+++ Assets/Javascript/activate-menu.js
@@ -1,9 +1,40 @@
-const menuButton = document.getElementById("mobile-menu-button")
-const menu = document.getElementById("navigation")
+const menuButton = document.getElementById("mobile-menu-button");
+const menu = document.getElementById("navigation");
+const socialHorizontal = document.querySelectorAll(".social-icons.horizontal .icon")
+const socialVertical = document.querySelectorAll(".social-icons.vertical .icon")
+
+
+
 
 menuButton.addEventListener('click', () => {
     menu.classList.toggle("active")
 });
 
-console.log(menuButton)
-console.log(menu)
+
+if(menu.contains("active")){
+
+    socialHorizontal.forEach(element => {
+        
+        element.classList.add("nonactive")
+    });
+}
+
+/*
+for (let i = 0; i < socialHorizontal.length; i++) {
+    
+    socialHorizontal[i].classList.toggle("nonactive")
+
+    socialHorizontal[i].style.color = "red"
+    
+    
+}
+for (let i = 0; i < socialVertical.length; i++) {
+    
+   number = socialVertical[i].classList.toggle("nonactive")
+
+    socialVertical[i].style.color = "red";
+
+}
+*/
+
+
